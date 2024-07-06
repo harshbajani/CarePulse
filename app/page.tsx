a417aa2c8a55fdedde4422633ec17a5618177a98
harshbajani <harshbajani7@gmail.com>
2024-07-06 11:23:55 +0530

    Folder, Theme, and Base Layout foundation setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="underline text-white">Home</h1>
+      <Button className="">gili gili</Button>
+    </div>
+  );
+}
